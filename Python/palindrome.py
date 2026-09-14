81406eb2c5a880985c171f187b043c94962bad08
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-09-14 15:31:56 +0530

    basic palindrome problem

---
+++ Python/palindrome.py
@@ -0,0 +1,11 @@
+### Check if a string is a palindrome
+
+
+s = "racecar" 
+
+if s==s[::-1]:
+    print("palindrome!")
+else:
+    print("not palindrome!")
+
+
--- Python/reverse_string.py
+++ Python/reverse_string.py
@@ -1,4 +1,16 @@
 ### Reverse a string
 
 string = "tanmay"
-print(string[::-1])
+print(string[::-1]) #### Start stop step
+print(string[2:-1])
+print(string[0:3])
+print(string[4:1:-1])
+
+'''
+
+String:   t   a   n   m   a   y
+Index:    0   1   2   3   4   5
+Negative: -6  -5  -4  -3  -2  -1
+
+
+'''
